ae52ba6a4be1bb5ff6cea5554a14ae0d84273da9
XiaYucca <1091976750@qq.com>
2017-05-11 10:09:45 +0800

    add voice view

---
+++ 小奥新版/VoiceView.m
@@ -0,0 +1,13 @@
+//
+//  VoiceView.m
+//  小奥新版
+//
+//  Created by RainPoll on 2017/5/11.
+//  Copyright © 2017年 RainPoll. All rights reserved.
+//
+
+#import "VoiceView.h"
+
+@implementation VoiceView
+
+@end
